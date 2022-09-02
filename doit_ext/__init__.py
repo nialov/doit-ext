e627c952c6b159a36185ca06af9962ac7fc96294
nialov <nikolasovaskainen@gmail.com>
2022-09-02 13:40:30 +0300

    build: bootstrap project from nialov-py-template

---
+++ doit_ext/__init__.py
@@ -0,0 +1,7 @@
+"""
+doit_ext.
+
+Utilities for doit task specification.
+"""
+
+__version__ = "0.0.0"
---
+++ tests/_profile.py
@@ -0,0 +1,14 @@
+"""
+Script for profiling ``doit_ext`` performance.
+"""
+
+
+def perf_profile():
+    """
+    Profile ``doit_ext`` performance.
+    """
+    return
+
+
+if __name__ == "__main__":
+    perf_profile()
