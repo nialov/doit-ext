0938087fa752df80a124fbc7052504a6589a44b5
nialov <nikolasovaskainen@gmail.com>
2022-11-02 17:22:57 +0200

    test(compose): test action composing

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -1,9 +1,12 @@
 """
 Test compose.py.
 """
+from contextlib import nullcontext as does_not_raise
 from pathlib import Path
+from typing import Optional
 
 import pytest
+from doit.task import Task, dict_to_task
 from doit.tools import config_changed, result_dep
 
 from doit_ext import compose
@@ -74,14 +77,27 @@ def test_composetask():
 
     mkdir_cmd = "mkdir -p tmp"
     mkdir_cmd_2 = "mkdir -p tmp2"
+    setup_py_path = Path("setup.py")
+    pyproject_toml_path = Path("pyproject.toml")
+    target_csv_path = Path("target.csv")
+    base_cmd = "python script.py {} {} {}"
+    option_param = "--csv"
+    file_dep_param = compose.FileDep(pyproject_toml_path)
+    target_param = compose.Target(target_csv_path)
     compose_task = (
-        compose.ComposeTask()
+        compose.ComposeTask(name="test_composetask_task")
         .add_actions(mkdir_cmd)
         .add_config_dependency(dict(x=2))
         .add_config_dependency(dict(y=4))
-        .add_file_deps("dodo.py", Path("setup.py"))
+        .add_file_deps("dodo.py", setup_py_path)
         .add_result_dep("other_task")
         .add_actions(mkdir_cmd_2)
+        .add_actions(
+            compose.Action(
+                base=base_cmd,
+                parameters=(option_param, file_dep_param, target_param),
+            )
+        )
     )
 
     compiled_compose_task = compose_task.compile()
@@ -97,6 +113,34 @@ def test_composetask():
             assert mkdir_cmd in values
             # Test that order is the same as inputted
             assert values.index(mkdir_cmd) < values.index(mkdir_cmd_2)
+            assert base_cmd not in values
+            assert (
+                base_cmd.format(option_param, file_dep_param.value, target_param.value)
+                in values
+            )
 
         if key == "uptodate":
             assert all(isinstance(val, (config_changed, result_dep)) for val in values)
+
+    assert isinstance(dict_to_task(compiled_compose_task), Task)
+
+
+@pytest.mark.parametrize(
+    "base,parameters,raises",
+    [
+        ("hello there {}!", ("Friend",), does_not_raise()),
+        ("hello there {} {} {}!", ("Friend", "And", "Another"), does_not_raise()),
+        ("hello there {}!", ("Friend", "Or", "Foe?"), pytest.raises(ValueError)),
+        ("hello there!", ("Friend",), pytest.raises(ValueError)),
+        ("hello there!", ("Friend", "Or", "Foe?"), pytest.raises(ValueError)),
+    ],
+)
+def test_action(base, parameters: tuple, raises):
+    """
+    Test Action NamedTuple.
+    """
+
+    with raises:
+        result = compose.Action(base=base, parameters=parameters).compile()
+        assert isinstance(result, str)
+        assert base[: base.index("{}")] in result
