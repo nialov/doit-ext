dc2ee715732b0e6e3ca5b26e55c949719abc8c98
nialov <nikolasovaskainen@gmail.com>
2023-06-27 15:55:46 +0300

    build: enable docs build with nix

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -37,9 +37,7 @@ release = imported_package.__version__  # type: ignore
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx_autodoc_typehints",
-    "sphinx_gallery.gen_gallery",
     "sphinx_rtd_theme",
-    "nbsphinx",
 ]
 
 # Add .md markdown files as sources.
@@ -47,12 +45,6 @@ source_suffix = {
     ".rst": "restructuredtext",
 }
 master_doc = "index"
-
-# Sphinx-gallery config
-sphinx_gallery_conf = {
-    "examples_dirs": "../examples",  # path to your example scripts
-    "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
-}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
--- flake.nix
+++ flake.nix
@@ -20,33 +20,55 @@
           overlays = [ self.overlays.default ];
         };
 
-        devShellPackages = with pkgs; [ pre-commit pandoc ];
+        devShellPackages = with pkgs; [ pre-commit pandoc poetry ];
 
       in {
-        packages.doit-ext = pkgs.python3Packages.doit-ext;
-        packages.doit-ext-python =
-          pkgs.python3.withPackages (p: with p; [ doit-ext doit ]);
-        # (with pkgs; {
-        # inherit (python3.pkgs)
-        #   buildPythonPackage pytestCheckHook poetry-core pytest-regressions;
-        # inherit (pkgs) poetry2nix;
-        # });
         checks = {
-          inherit (self.packages."${system}") doit-ext;
+          inherit (self.packages."${system}") doit-ext doit-ext-docs;
           preCommitCheck = inputs.pre-commit-hooks.lib.${system}.run
             (import ././pre-commit.nix { inherit pkgs; });
 
         };
-        packages.default = self.packages."${system}".doit-ext;
-        devShells.default = pkgs.mkShell {
-          packages = devShellPackages;
-          inherit (self.checks.${system}.preCommitCheck) shellHook;
-
+        packages = {
+          default = self.packages."${system}".doit-ext;
+          poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+            editablePackageSources = { doit_ext = ./doit_ext; };
+          };
+          inherit (pkgs.python3Packages) doit-ext;
+          doit-ext-python =
+            pkgs.python3.withPackages (p: with p; [ doit-ext doit ]);
+          doit-ext-docs = let
+            sphinxEnv = pkgs.python3.withPackages (p:
+              with p; [
+                doit-ext
+                sphinx
+                sphinx-autodoc-typehints
+                sphinx-rtd-theme
+              ]);
+          in pkgs.runCommand "doit-ext-docs" { } ''
+            tmpdir=$(mktemp -d)
+            ln -s ${./doit_ext} $tmpdir/doit_ext
+            cp -r ${./docs_src} $tmpdir/docs_src
+            chmod -R 777 $tmpdir/docs_src
+            cd $tmpdir
+            ${sphinxEnv}/bin/sphinx-apidoc -o docs_src/apidoc -f doit_ext -e -f
+            ${sphinxEnv}/bin/sphinx-build -b html docs_src/ $out
+          '';
+        };
+        devShells = {
+          default = pkgs.mkShell {
+            packages = devShellPackages;
+            inherit (self.checks.${system}.preCommitCheck) shellHook;
+          };
+          poetry = self.packages."${system}".poetryEnv.env;
         };
       }) // {
         overlays.default = _: prev: {
           pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-            (python-final: _: { doit-ext = python-final.callPackage ./. { }; })
+            (python-final: _: {
+              doit-ext = python-final.callPackage ./default.nix { };
+            })
           ];
         };
       };
