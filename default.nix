1938682b60f44d59efb106602fa05b1fc0996168
nialov <nikolasovaskainen@gmail.com>
2023-06-27 13:37:00 +0300

    build: remove python build tooling

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, python, pytestCheckHook, doit, poetry-core
+{ lib, buildPythonPackage, pytestCheckHook, doit, poetry-core
 , pytest-regressions, poetry2nix }:
 
 buildPythonPackage {
@@ -15,21 +15,13 @@ buildPythonPackage {
       (builtins.any (value: lib.hasSuffix value (baseNameOf path))
         excludeSuffixes);
 
-  in builtins.filterSource (path: type:
+  in builtins.filterSource (path: _:
     # Apply the anyMatches filter and reverse the result with !
     # as we want to EXCLUDE rather than INCLUDE
     !(anyMatches path)) (poetry2nix.cleanPythonSources { src = ./.; });
 
   nativeBuildInputs = [ poetry-core ];
   propagatedBuildInputs = [ doit ];
-
-  # The cythonized extensions are required to exist in the pygeos/ directory
-  # for the package to function. Therefore override of buildPhase was
-  # necessary.
-  # buildPhase = ''
-  #   ${python.interpreter} setup.py build_ext --inplace
-  #   ${python.interpreter} setup.py bdist_wheel
-  # '';
 
   checkInputs = [ pytestCheckHook pytest-regressions ];
 
